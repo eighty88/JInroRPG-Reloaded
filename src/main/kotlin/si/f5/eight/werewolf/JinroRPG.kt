0b8dbd09783fec5ddc278131f46ac868cd52b510
eight_y_88 <jadec.eight@gmail.com>
2022-01-23 18:59:43 +0900

    Initial Commit

---
+++ src/main/kotlin/si/f5/eight/werewolf/JinroRPG.kt
@@ -0,0 +1,29 @@
+package si.f5.eight.werewolf
+
+import org.bukkit.Bukkit
+import org.bukkit.event.Listener
+import org.bukkit.plugin.java.JavaPlugin
+
+class JinroRPG : JavaPlugin() {
+    companion object {
+        lateinit var INSTANCE: JinroRPG private set
+    }
+
+    override fun onEnable() {
+        INSTANCE = this
+
+        registerEventListeners(
+
+        )
+    }
+
+    override fun onDisable() {
+
+    }
+
+    fun registerEventListeners(vararg listeners: Listener) {
+        listeners.forEach {
+            Bukkit.getPluginManager().registerEvents(it, this)
+        }
+    }
+}
