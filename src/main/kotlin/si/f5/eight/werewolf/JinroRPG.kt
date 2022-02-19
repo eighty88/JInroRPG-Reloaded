899feccae84a0ce71764200c6782f0ace6a18fc9
eight_y_88 <jadec.eight@gmail.com>
2022-02-19 09:01:04 +0900

    Update

--- src/main/kotlin/si/f5/eight/werewolf/JinroRPG.kt
+++ src/main/kotlin/si/f5/eight/werewolf/JinroRPG.kt
@@ -3,6 +3,7 @@ package si.f5.eight.werewolf
 import org.bukkit.Bukkit
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
+import si.f5.eight.werewolf.command.AbstractCommand
 
 class JinroRPG : JavaPlugin() {
     companion object {
@@ -25,5 +26,10 @@ class JinroRPG : JavaPlugin() {
         listeners.forEach {
             Bukkit.getPluginManager().registerEvents(it, this)
         }
+    }
+
+    private fun registerCommands(name: String, command: AbstractCommand) {
+        getCommand(name)?.setExecutor(command)
+        getCommand(name)?.tabCompleter = command
     }
 }
---
+++ src/main/kotlin/si/f5/eight/werewolf/command/AbstractCommand.kt
@@ -0,0 +1,6 @@
+package si.f5.eight.werewolf.command
+
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.TabCompleter
+
+abstract class AbstractCommand : CommandExecutor, TabCompleter {}
